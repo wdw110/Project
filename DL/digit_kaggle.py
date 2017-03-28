492b409d7dbe4d8f67888ea88b810d9dab70aa6d
wdw110 <wudw110@gmail.com>
2017-03-28 14:58:28 +0800

    fix digit_kaggle.py

--- DL/digit_kaggle.py
+++ DL/digit_kaggle.py
@@ -2,33 +2,18 @@
 #author: wdw110
 #Classification of kaggle digits recongnize by convolutional neural networks
 
-import os,random
-import pandas as pd
+import keras
 import numpy as np
-import theano as th
-import theano.tensor as T
-from keras.utils import np_utils
-import keras.models as models
-from keras.layers import Input,merge
-from keras.layers.core import Reshape,Dense,Dropout,Activation,Flatten,MaxoutDense
+import pandas as pd
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
 from keras.layers.advanced_activations import LeakyReLU
-from keras.activations import *
-from keras.layers.wrappers import TimeDistributed
-from keras.layers.noise import GaussianNoise
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D, UpSampling2D
-from keras.layers.recurrent import LSTM
-from keras.regularizers import *
-from keras.layers.normalization import *
-from keras.optimizers import *
-from keras.datasets import mnist
-#import matplotlib.pyplot as plt
-
-import cPickle, random, sys, keras
-from keras.models import Model
-from IPython import display
+from keras.layers.core import Dense,Dropout,Activation,Flatten
+from keras.callbacks import ModelCheckpoint
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
+from keras.optimizers import *
 
-from keras.models import load_model
 
 data = pd.read_csv('/Users/wdw/Desktop/test/Kaggle_data/digit_recongnizer/train.csv').values
 d_train = data[0:40000,:]
@@ -42,24 +27,30 @@ y_train = d_train[:,0]
 X_test = d_test[:,1:].reshape(n, 28, 28)
 y_test = d_test[:,0]
 
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+dropout_rate = 0.25
+opt = Adam(lr=1e-4)
+dopt = Adam(lr=1e-3)
+
 img_rows, img_cols = 28, 28
 
 #the data, shuffled and split between train and test sets
-path = os.path.join(os.getcwd(),'mnist_little.pkl.gz')
+#path = os.path.join(os.getcwd(),'mnist_little.pkl.gz')
 #(X_train, y_train), (X_test, y_test) = mnist.load_data(path)
 
-X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
 Shape=X_train.shape[1:]
-X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-d_pre = d_pre.reshape(d_pre.shape[0], 1, img_rows, img_cols)
+X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+d_pre = d_pre.reshape(d_pre.shape[0], img_rows, img_cols, 1)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 d_pre = d_pre.astype('float32')
 X_train /= 255
 X_test /= 255
 d_pre /= 255
-
-
 
 print np.min(X_train), np.max(X_train)
 
@@ -72,11 +63,6 @@ print y_train.shape
 print y_test.shape
 print '************************'
 
-shp = X_train.shape[1:]
-
-dropout_rate = 0.25
-opt = Adam(lr=1e-4)
-dopt = Adam(lr=1e-3)
 
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
@@ -84,51 +70,73 @@ y_test = np_utils.to_categorical(y_test)
 class_num=y_train.shape[1]
    
 
-# discriminator 
+# Parametrize the image augmentation class
+datagen = ImageDataGenerator(
+	rotation_range = 20,
+	width_shift_range = 0.15,
+	height_shift_range = 0.15,
+	shear_range = 0.4,
+	zoom_range = 0.3,                    
+	channel_shift_range = 0.1) 
 
-d_input = Input(shape=shp)
-H = Convolution2D(256, 5, 5, subsample=(2, 2), border_mode = 'same', activation='relu')(d_input)
-H = LeakyReLU(0.2)(H)
-H = Dropout(dropout_rate)(H)
-H = Convolution2D(512, 5, 5, subsample=(2, 2), border_mode = 'same', activation='relu')(H)
-H = LeakyReLU(0.2)(H)
-H = Dropout(dropout_rate)(H)
-H = Flatten()(H)
-H = Dense(256)(H)
-H = LeakyReLU(0.2)(H)
-H = Dropout(dropout_rate)(H)
-Features=H
-d_V = Dense(class_num,activation='softmax')(H)
-discriminator = Model(d_input,d_V)
-discriminator.compile(loss='binary_crossentropy', optimizer=dopt,metrics=['accuracy'])
+## CNN MODEL ##
+model = Sequential()
 
+model.add(Convolution2D(256, 5, 5, subsample=(2, 2), border_mode = 'same', activation='relu', input_shape=(img_rows, img_cols, 1)))
+model.add(LeakyReLU(0.2))
 
-#discriminator.summary()
-discriminator.fit(X_train,y_train ,nb_epoch=1, batch_size=128,verbose=1)
-Extractor=Model(d_input,Features)
+model.add(Dropout(dropout_rate))
 
+model.add(Convolution2D(512, 5, 5, subsample=(2, 2), border_mode = 'same', activation='relu'))
+model.add(LeakyReLU(0.2))
 
-Ftr = Extractor.predict(X_train) # Ftr is 256 dim
-Fts = Extractor.predict(X_test)
-Ftp = Extractor.predict(d_pre)
+model.add(Dropout(dropout_rate))
 
-import keras 
-adm=keras.optimizers.Adam(lr=0.2, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+model.add(Flatten())
+model.add(Dense(256))
+model.add(LeakyReLU(0.2))
+model.add(Dropout(dropout_rate))
+model.add(Dense(num_classes, activation='softmax'))
 
 
-input_v=Input(shape=[256])
-output_v=Dense(10,activation='sigmoid',init='glorot_normal')(input_v)
-Transformer= Model(input_v, output_v)
-Transformer.compile(optimizer=adm, loss='binary_crossentropy',metrics=['accuracy'])
-Transformer.fit(Ftr, y_train, validation_data=(Fts, y_test),nb_epoch=2000,batch_size=1000,shuffle=True,verbose=1)
+## LEARNING ##
 
-scores = Transformer.evaluate(Fts, y_test, verbose=0)
-print("Baseline Error X_test test: %.2f%%" % (100-scores[1]*100))
+# First, use AdaDelta for some epochs because AdaMax gets stuck
+model.compile(loss='categorical_crossentropy',
+			  optimizer='adadelta', 
+			  metrics=['accuracy'])
 
-classes = Transformer.predict(Ftp)
-result = classes.argmax(axis=1)
-df = pd.DataFrame(result,index=range(1,classes.shape[0]+1),columns=['Label'])
-df.to_csv('submission.csv')
+# Fit
+model.fit(X_train, y_train, batch_size=batch_size,
+		  nb_epoch=epochs,
+		  validation_data=(X_test,y_test),
+		  verbose=1)
 
-print type(classes),classes.shape
+# Now, use AdaMax
+model.compile(loss='categorical_crossentropy',
+			  optimizer='adamax', 
+			  metrics=['accuracy'])
+
+# We want to keep the best model. This callback will store
+# in a file the weights of the model with the highest validation accuracy
+saveBestModel = ModelCheckpoint('best.kerasModelWeights', monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=True)
+
+# Make the model learn using the image generator
+model.fit_generator(datagen.flow(X_train, y_train, batch_size=batch_size),
+					samples_per_epoch=len(X_train),
+					nb_epoch=epochs,
+					validation_data=(X_test,y_test),
+					callbacks=[saveBestModel],
+					verbose=1)
+
+## PREDICTON ##
+
+# Load the model with  the hightest validation accuracy
+model.load_weights('best.kerasModelWeights')
+
+# Predict the class (give the index in the one-hot vector of the most probable class)
+Y_pred = model.predict_classes(d_pre)
+
+# Save the predicitions in Kaggle format
+np.savetxt("CNN_pred.csv", np.c_[range(1,len(d_pre)),Y_pred], delimiter=',', header = 'ImageId,Label', comments = '', fmt='%s')
 
